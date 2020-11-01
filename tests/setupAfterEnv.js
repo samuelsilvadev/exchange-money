750bcf17792700069038d26c38be7b30d55420ce
Samuel Silva <samuelsilvawb@gmail.com>
2020-11-01 14:09:24 +0100

    chore: Update test environment configs and add patch to razzle tooling
    - Added MutationObserver on test environment and removed unnecessary jest-environment-jsdom-sixteen package.
    - Due to a css config problem on razzle tooling we weren't getting the correct classnames when building the server,
    so it was necessary to add a patch locally to proceed with the current delivery.
    - Added .env.sample.

--- tests/setupAfterEnv.js
+++ tests/setupAfterEnv.js
@@ -1,4 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { cleanup } from '@testing-library/react';
+import MutationObserver from 'mutation-observer';
 
 afterEach(cleanup);
+
+global.MutationObserver = MutationObserver;
