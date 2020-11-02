afb08afdf05fadabd8392ed0a990f81055443e70
Samuel Silva <samuelsilvawb@gmail.com>
2020-11-02 22:25:13 +0000

    chore: Add github actions config
    - Added push master workflow.
    - Added Procfile to change heroku default script.
    - Added razzle-heroku plugin on razzle config.
    - Updated .editorconfig settings to yml files.
    - Moved razzle, razzle-heroku and prop-types to prod dependencies in order to build correctly on heroku.

---
+++ razzle.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const razzleHeroku = require('razzle-heroku');
+
+module.exports = {
+	modifyWebpackConfig(opts) {
+		let config = opts.webpackConfig;
+
+		config = razzleHeroku(
+			config,
+			{ target: opts.env.target, dev: opts.env.dev },
+			opts.webpackObject
+		);
+
+		return config;
+	},
+};
